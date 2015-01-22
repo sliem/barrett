08315d1082ac55a0dc3494c017513fed6ca41ec0
Sebastian Liem <sebastian@liem.se>
2015-01-22 16:51:58 +0100

    Improved the example code.

--- example/plot.py
+++ example/plot.py
@@ -13,14 +13,16 @@ def main():
     dataset = 'RD'
 
     #logObservables(dataset)
+    #calculateC(dataset)
+    #logC(dataset)
 
     mass = ['log(m_{\chi})']
-    coefficients = ['D_1', 'D_2', 'D_3', 'D_4', 'D_5', 'D_6']
+    C = ['log(C_1)', 'log(C_2)', 'log(C_3)', 'log(C_4)', 'log(C_5)', 'log(C_6)']
     observables = ['log(<\sigma v>)', 'log(\Omega_{\chi}h^2)', 'log(\sigma_p^{SI})']
 
-    plot1D(dataset, mass + coefficients + observables, 'histograms')
-    plot_coefficients(dataset, coefficients)
-    plot_vs_mass(dataset, coefficients, 'mass_vs_coefficients')
+    plot_histograms(dataset, mass + C + observables, 'histograms')
+    plot_coefficients(dataset, C, 'coefficients')
+    plot_vs_mass(dataset, C, 'mass_vs_coefficients')
     plot_vs_mass(dataset, observables, 'mass_vs_observables')
 
 
@@ -32,7 +34,43 @@ def logObservables(dataset):
 		d.log(v)
 
 
-def plot_coefficients(dataset, vars, nbins=60):
+def calculateC(dataset):
+    
+    def DtoC(D, logm, k):
+        return np.sign(D) * k * np.power(10, -np.abs(D) - 2 * logm)
+
+    def DtoC1234(D, logm):
+        k = 39.4784176044
+        return DtoC(D, logm, k)
+
+    def DtoC56(D, logm):
+        k = 1.16856116108
+        return DtoC(D, logm, k)
+
+    d = data.Chain(dataset+'.h5')
+
+    for n in ['1', '2', '3', '4']:
+        d.apply('C_'+n, 'GeV^{-2}', DtoC1234, 'D_'+n, 'log(m_{\chi})')
+
+    for n in ['5', '6']:
+        d.apply('C_'+n, 'GeV^{-2}', DtoC56, 'D_'+n, 'log(m_{\chi})')
+
+
+def logC(dataset):
+    def log(x):
+
+        if np.abs(x) > 1:
+            return 1e10
+        
+        return np.sign(x) * np.log10(np.abs(x))
+
+    d = data.Chain(dataset+'.h5')
+
+    for n in ['C_1', 'C_2', 'C_3', 'C_4', 'C_5', 'C_6']:
+        d.apply('log(%s)' % n, 'log(GeV^{-2})', np.vectorize(log, otypes=[np.float64]), n) 
+
+
+def plot_coefficients(dataset, vars, filename, nbins=60):
 
     if not os.path.isdir(dataset):
          os.mkdir(dataset)
@@ -46,21 +84,17 @@ def plot_coefficients(dataset, vars, nbins=60):
 
     plt.subplots_adjust(wspace=0, hspace=0)
 
-    ticks = [-15, -10, -5, 0, 5, 10, 15]
-
     for i, y in enumerate(vars):
         for j, x in enumerate(vars):
 
             ax = axes[i,j]
-            ax.set_xticks(ticks)
-            ax.set_yticks(ticks)
 
             if j > i:
                 # No need to do the analysis twice. (a[i,j] == a[j,i].T)
                 ax.set_axis_off()
 
             elif i == j:
-                P = posterior.oneD(dataset+'.h5', x, nbins)
+                P = posterior.oneD(dataset+'.h5', x, limits=(-30,30), nbins=nbins)
                 P.marginalise()
                 P.plot(ax)
                 ax.set_title(P.var)
@@ -70,7 +104,7 @@ def plot_coefficients(dataset, vars, nbins=60):
                 	ax.set_ylabel(ax.get_xlabel())
 
             else:
-                P = posterior.twoD(dataset+'.h5', x, y, nbins)
+                P = posterior.twoD(dataset+'.h5', x, y, xlimits=(-30,30), ylimits=(-30,30), nbins=nbins)
                 P.marginalise()
                	P.plot(ax)
 
@@ -80,7 +114,7 @@ def plot_coefficients(dataset, vars, nbins=60):
             	ax.set_ylabel('')
 
     fig.set_size_inches(n*3+1,n*3+1)
-    fig.savefig('%s/coefficients.png' % dataset, dpi=100)
+    fig.savefig('%s/%s.png' % (dataset, filename), dpi=100)
 
     plt.close(fig)
 
@@ -101,8 +135,13 @@ def plot_vs_mass(dataset, vars, filename, nbins=60):
 
     for i, y in enumerate(vars):
         ax = axes[i]
-		
-        P = posterior.twoD(dataset+'.h5', 'log(m_{\chi})', y, nbins)
+
+        if y[0:6] == 'log(C_':
+            ylimits = (-30, 30)
+        else:
+            ylimits = None	
+
+        P = posterior.twoD(dataset+'.h5', 'log(m_{\chi})', y, ylimits=ylimits, nbins=nbins)
         P.marginalise()
         P.plot(ax)
 
@@ -112,7 +151,7 @@ def plot_vs_mass(dataset, vars, filename, nbins=60):
     plt.close(fig)
 
 
-def plot1D(dataset, vars, filename, nbins=60):
+def plot_histograms(dataset, vars, filename, nbins=60):
 
     if not os.path.isdir(dataset):
          os.mkdir(dataset)
@@ -129,7 +168,12 @@ def plot1D(dataset, vars, filename, nbins=60):
     for i, x in enumerate(vars):
         ax = axes[i]
 
-        P = posterior.oneD(dataset+'.h5', x, nbins)
+        if x[0:6] == 'log(C_':
+            limits = (-30, 30)
+        else:
+            limits = None
+
+        P = posterior.oneD(dataset+'.h5', x, limits=limits, nbins=nbins)
         P.marginalise()
         P.plot(ax)
 
@@ -143,3 +187,4 @@ def plot1D(dataset, vars, filename, nbins=60):
 
 if __name__ == '__main__':
 	main()
+
