977f2b0372ed8515fa44ff3e054c1a9add82e5af
Sebastian Liem <sebastian.liem@uva.nl>
2016-01-07 11:53:17 +0100

    Reimplemented the profile likelihood using scipy.stats.binned_statistic. It's now faster, shorter and less buggy.

--- profilelikelihood.py
+++ profilelikelihood.py
@@ -1,11 +1,10 @@
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import scipy.ndimage as ndimage
 import scipy.stats as stats
 import h5py
 import barrett.util as util
+
 
 class oneD:
     """ Calculate and plot the one dimensional profile likelihood.
@@ -15,65 +14,39 @@ class oneD:
         self.h5file = h5file
         self.var = var
 
-        with h5py.File(h5file, 'r') as f:
-            self.n = f[self.var].shape[0]
-            self.name = f[self.var].name
-            self.unit = f[self.var].attrs['unit'].decode('utf8')
+        h5 = h5py.File(h5file, 'r')
+
+        self.n = h5[self.var].shape[0]
+        self.name = h5[self.var].name
+        self.unit = h5[self.var].attrs['unit'].decode('utf8')
+        self.chunksize = h5[self.var].chunks[0]
 
         self.min, self.max, self.mean = util.threenum(self.h5file, self.var)
+        self.nbins = np.floor(self.n**1/2) if nbins is None else nbins
+        self.limits = (self.min, self.max) if limits is None else limits
 
-        if limits is None:
-            self.limits = (0.99*self.min, 1.01*self.max)
-        else:
-            self.limits = limits
-
-        if nbins != None:
-            self.nbins = nbins
-        else:
-            self.nbins = np.floor(self.n**1/2)
-
-        self.bin_edges  = np.linspace(self.limits[0], self.limits[1], num=self.nbins+1)
-        self.bin_widths = np.diff(self.bin_edges)
-
-
-    def profile(self):
-
-        f = h5py.File(self.h5file, 'r')
-        x = f[self.var]
-        l = f['-2lnL']
-        s = x.chunks[0]
-
-        bins = 1e100*np.ones(self.nbins)
-
+        chisq = np.zeros(self.nbins) + 1e100
+        s = self.chunksize
         for i in range(0, self.n, s):
-            lc = l[i:i+s]
-            xc = x[i:i+s]
+            r = stats.binned_statistic(h5[self.var][i:i+s], h5['-2lnL'][i:i+s], np.nanmin,
+                                       bins=self.nbins, range=self.limits)
+            chisq = np.fmin(chisq, r.statistic)
 
-            # filter out outliers
-            not_outliers = np.all([xc >= self.limits[0], xc <= self.limits[1]], axis=0)
-            lc = lc[not_outliers]
-            xc = xc[not_outliers]
+        self.proflike = np.exp(-(chisq - chisq.min())/2)
 
-            bin_index = np.digitize(xc, self.bin_edges, right=True) - 1
+        self.bin_edges  = r.bin_edges
 
-            for j in range(lc.shape[0]):
-                if lc[j] < bins[bin_index[j]]:
-                    bins[bin_index[j]] = lc[j]
-
-        self.profchisq = bins - bins.min()
-        self.proflike = np.exp(-self.profchisq/2)
-
-        f.close()
+        h5.close()
 
 
     def plot(self, ax):
 
         ax.hist(self.bin_edges[:-1],
-                 bins=self.bin_edges,
-                 weights=self.proflike,
-                 histtype='stepfilled',
-                 alpha=0.5,
-                 color='green')
+                bins=self.bin_edges,
+                weights=self.proflike,
+                histtype='stepfilled',
+                alpha=0.5,
+                color='green')
 
         ax.set_ylim(0, self.proflike.max()*1.1)
         ax.set_xlabel('%s [%s]' % (self.name, self.unit))
@@ -83,99 +56,60 @@ class twoD:
     """ Calculate and plot the two dimensional profile likelihood.
     """
 
-    def __init__(self, h5file, xvar, yvar, xlimits=None, ylimits=None, zero=False, nbins=None):
+    def __init__(self, h5file, xvar, yvar, xlimits=None, ylimits=None, nbins=None):
 
         self.h5file = h5file
         self.xvar = xvar
         self.yvar = yvar
 
-        with h5py.File(h5file, 'r') as f:
-            self.n = f[self.xvar].shape[0]
-            self.xname = f[self.xvar].name
-            self.xunit = f[self.xvar].attrs['unit'].decode('utf8')
-            self.yname = f[self.yvar].name
-            self.yunit = f[self.yvar].attrs['unit'].decode('utf8')
+        h5 = h5py.File(h5file, 'r')
+
+        self.n = h5[self.xvar].shape[0]
+        self.chunksize = h5[self.xvar].chunks[0]
+        self.xname = h5[self.xvar].name
+        self.xunit = h5[self.xvar].attrs['unit'].decode('utf8')
+        self.yname = h5[self.yvar].name
+        self.yunit = h5[self.yvar].attrs['unit'].decode('utf8')
 
         self.xmin, self.xmax, self.xmean = util.threenum(self.h5file, self.xvar)
         self.ymin, self.ymax, self.ymean = util.threenum(self.h5file, self.yvar)
 
-        if xlimits is None:
-            self.xlimits = (0.99*self.xmin, 1.01*self.xmax)
+        self.nbins = np.floor(self.n**1/2) if nbins is None else nbins
+        self.xlimits = (self.xmin, self.xmax) if xlimits is None else xlimits
+        self.ylimits = (self.ymin, self.ymax) if ylimits is None else ylimits
 
-            if zero:
-                self.xlimits = (0.0, self.xlimits[1])
-
-        else:
-            self.xlimits = xlimits
-
-        if ylimits is None:
-            self.ylimits = (0.99*self.ymin, 1.01*self.ymax)
-
-            if zero:
-                self.ylimits = (0.0, self.ylimits[1])
-
-        else:
-            self.ylimits = ylimits
-
-        if nbins != None:
-            self.nbins = nbins
-        else:
-            self.nbins = np.floor(self.n**1/2)
-
-        self.xbin_edges  = np.linspace(self.xlimits[0], self.xlimits[1], num=self.nbins+1)
-        self.ybin_edges  = np.linspace(self.ylimits[0], self.ylimits[1], num=self.nbins+1)
-
-        self.xbin_widths = np.diff(self.xbin_edges)
-        self.ybin_widths = np.diff(self.ybin_edges)
-
-        self.bins = None
-
-
-    def profile(self):
-
-        f = h5py.File(self.h5file, 'r')
-        x = f[self.xvar]
-        y = f[self.yvar]
-        l = f['-2lnL']
-        s = x.chunks[0]
-
-        bins = 1e100*np.ones((self.nbins, self.nbins))
-
+        chisq = np.zeros((self.nbins, self.nbins)) + 1e100
+        s = self.chunksize
         for i in range(0, self.n, s):
-            for j in range(i,s):
-                if x[j] < self.xlimits[0] or x[j] > self.xlimits[1]:
-                    continue
+            r = stats.binned_statistic_2d(h5[self.xvar][i:i+s],
+                                       h5[self.yvar][i:i+s],
+                                       h5['-2lnL'][i:i+s],
+                                       np.nanmin, bins=self.nbins,
+                                       range=[self.xlimits, self.ylimits])
+            chisq = np.fmin(chisq, r.statistic)
+        chisq = chisq.T
+        self.proflike = np.exp(-(chisq - chisq.min())/2)
 
-                if y[j] < self.ylimits[0] or y[j] > self.ylimits[1]:
-                    continue
+        self.xbin_edges  = r.x_edge
+        self.ybin_edges  = r.y_edge
+        self.xcenters = self.xbin_edges[:-1] + np.diff(self.xbin_edges)/2
+        self.ycenters = self.ybin_edges[:-1] + np.diff(self.ybin_edges)/2
 
-                xbin_index = np.floor((x[j]-self.xlimits[0])/self.xbin_widths[0])
-                ybin_index = np.floor((y[j]-self.ylimits[0])/self.ybin_widths[0])
-
-                if l[j] < bins[ybin_index, xbin_index]:
-                    bins[ybin_index, xbin_index] = l[j]
-
-        self.profchisq = bins - bins.min()
-        self.proflike = np.exp(-self.profchisq/2)
-
-        f.close()
+        h5.close()
 
 
-    def plot(self, ax, levels=[0.95, 0.68], smoothing=False):
+    def plot(self, ax, levels=[0.95, 0.68]):
 
-        xcenter = self.xbin_edges[:-1] + self.xbin_widths/2
-        ycenter = self.ybin_edges[:-1] + self.ybin_widths/2
+        X, Y = np.meshgrid(self.xcenters, self.ycenters)
 
-        X, Y = np.meshgrid(xcenter, ycenter)
-
-        cmap = matplotlib.cm.gist_heat_r
-
-        if levels == None:
+        if levels is None:
             levels = np.linspace(0, self.proflike.max(), 10)[1:]
         else:
-            levels = list(self.confidenceregions(levels)) + [self.proflike.max()]
+            levels = np.append(self.confidenceregions(levels), self.proflike.max())
 
+        cmap = matplotlib.cm.gist_heat_r
         colors = [cmap(i) for i in np.linspace(0.2,0.8,len(levels))][1:]
+
         ax.contourf(X, Y, self.proflike, levels=levels, colors=colors)
 
         ax.set_xlabel('%s [%s]' % (self.xname, self.xunit))
@@ -183,7 +117,5 @@ class twoD:
 
 
     def confidenceregions(self, probs):
-
         deltachi2 = stats.chi2.ppf(probs, 2)
-
         return np.exp(-deltachi2/2)
