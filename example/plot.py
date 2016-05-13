4b4f28ca394344d7174fd3a0b5113d5ffabe9c1c
Sebastian Liem <sebastian.liem@uva.nl>
2016-05-13 14:09:53 +0200

    Fixed and updated the example slightly. The dataset used is still terrible.

--- example/plot.py
+++ example/plot.py
@@ -1,129 +1,36 @@
 #!/usr/bin/env python
 
-import os
 import barrett.posterior as posterior
 import barrett.data as data
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
 
 def main():
-
+    """ We here demostrate the basic functionality of barrett. We use a global scan 
+    of scalar dark matter as an example. The details aren't really important.
+    """
     dataset = 'RD'
 
-    #logObservables(dataset)
-    #calculateC(dataset)
-    #logC(dataset)
+    observables = ['log(<\sigma v>)', '\Omega_{\chi}h^2', 'log(\sigma_p^{SI})']
+    var  = ['log(m_{\chi})']
+    var += ['log(C_1)', 'log(C_2)', 'log(C_3)', 'log(C_4)', 'log(C_5)', 'log(C_6)']
+    var += observables
 
-    mass = ['log(m_{\chi})']
-    C = ['log(C_1)', 'log(C_2)', 'log(C_3)', 'log(C_4)', 'log(C_5)', 'log(C_6)']
-    observables = ['log(<\sigma v>)', 'log(\Omega_{\chi}h^2)', 'log(\sigma_p^{SI})']
-
-    plot_histograms(dataset, mass + C + observables, 'histograms')
-    plot_coefficients(dataset, C, 'coefficients')
-    plot_vs_mass(dataset, C, 'mass_vs_coefficients')
-    plot_vs_mass(dataset, observables, 'mass_vs_observables')
+    plot_vs_mass(dataset, observables, 'mass_vs_observables.png')
+    plot_oneD(dataset, var, 'oneD.png')
 
 
-def logObservables(dataset):
+def plot_vs_mass(dataset, vars, filename, bins=60):
+    """ Plot 2D marginalised posteriors of the 'vars' vs the dark matter mass.
+    We plot the one sigma, and two sigma filled contours. More contours can be plotted 
+    which produces something more akin to a heatmap.
 
-	d = data.Chain(dataset+'.h5')
+    If one require more complicated plotting, it is recommended to write a custom 
+    plotting function by extending the default plot() method.
+    """
 
-	for v in ['<\sigma v>', '\Omega_{\chi}h^2', '\sigma_p^{SI}']:
-		d.log(v)
-
-
-def calculateC(dataset):
-    
-    def DtoC(D, logm, k):
-        return np.sign(D) * k * np.power(10, -np.abs(D) - 2 * logm)
-
-    def DtoC1234(D, logm):
-        k = 39.4784176044
-        return DtoC(D, logm, k)
-
-    def DtoC56(D, logm):
-        k = 1.16856116108
-        return DtoC(D, logm, k)
-
-    d = data.Chain(dataset+'.h5')
-
-    for n in ['1', '2', '3', '4']:
-        d.apply('C_'+n, 'GeV^{-2}', DtoC1234, 'D_'+n, 'log(m_{\chi})')
-
-    for n in ['5', '6']:
-        d.apply('C_'+n, 'GeV^{-2}', DtoC56, 'D_'+n, 'log(m_{\chi})')
-
-
-def logC(dataset):
-    def log(x):
-
-        if np.abs(x) > 1:
-            return 1e10
-        
-        return np.sign(x) * np.log10(np.abs(x))
-
-    d = data.Chain(dataset+'.h5')
-
-    for n in ['C_1', 'C_2', 'C_3', 'C_4', 'C_5', 'C_6']:
-        d.apply('log(%s)' % n, 'log(GeV^{-2})', np.vectorize(log, otypes=[np.float64]), n) 
-
-
-def plot_coefficients(dataset, vars, filename, nbins=60):
-
-    if not os.path.isdir(dataset):
-         os.mkdir(dataset)
-    
-    n = len(vars)
-
-    fig, axes = plt.subplots(nrows=n,
-                             ncols=n,
-                             sharex='col',
-                             sharey=False) # The histograms otherwise become terrible.
-
-    plt.subplots_adjust(wspace=0, hspace=0)
-
-    for i, y in enumerate(vars):
-        for j, x in enumerate(vars):
-
-            ax = axes[i,j]
-
-            if j > i:
-                # No need to do the analysis twice. (a[i,j] == a[j,i].T)
-                ax.set_axis_off()
-
-            elif i == j:
-                P = posterior.oneD(dataset+'.h5', x, limits=(-30,30), nbins=nbins)
-                P.marginalise()
-                P.plot(ax)
-                ax.set_title(P.var)
-
-                if i == 0:
-                	ax.set_yticklabels([])
-                	ax.set_ylabel(ax.get_xlabel())
-
-            else:
-                P = posterior.twoD(dataset+'.h5', x, y, xlimits=(-30,30), ylimits=(-30,30), nbins=nbins)
-                P.marginalise()
-               	P.plot(ax)
-
-            if j != 0: 
-                # remove labels from inner 
-            	ax.set_yticklabels([])
-            	ax.set_ylabel('')
-
-    fig.set_size_inches(n*3+1,n*3+1)
-    fig.savefig('%s/%s.png' % (dataset, filename), dpi=100)
-
-    plt.close(fig)
-
-
-def plot_vs_mass(dataset, vars, filename, nbins=60):
-
-    if not os.path.isdir(dataset):
-         os.mkdir(dataset)
-    
     n = len(vars)
 
     fig, axes = plt.subplots(nrows=n,
@@ -133,29 +40,29 @@ def plot_vs_mass(dataset, vars, filename, nbins=60):
 
     plt.subplots_adjust(wspace=0, hspace=0)
 
+    m = 'log(m_{\chi})'
+
     for i, y in enumerate(vars):
         ax = axes[i]
+        P = posterior.twoD(dataset+'.h5', m, y, 
+                           xlimits=limits(m), ylimits=limits(y), xbins=bins, ybins=bins)
 
-        if y[0:6] == 'log(C_':
-            ylimits = (-30, 30)
-        else:
-            ylimits = None	
+        # apply some gaussian smoothing to make the contours slightly smoother
+        sigmas = (np.diff(P.ycenters)[0], np.diff(P.xcenters)[0])
+        P.pdf = gaussian_filter(P.pdf, sigmas, mode='nearest')
 
-        P = posterior.twoD(dataset+'.h5', 'log(m_{\chi})', y, ylimits=ylimits, nbins=nbins)
-        P.marginalise()
-        P.plot(ax)
+        P.plot(ax, levels=np.linspace(0.9, 0.1, 9))
+        ax.set_xlabel(labels('log(m_{\chi})'))
+        ax.set_ylabel(labels(y))
 
-    fig.set_size_inches(8,n*6)
-    fig.savefig('%s/%s.png' % (dataset, filename), dpi=100)
+    fig.set_size_inches(4,n*3)
+    fig.savefig(filename, dpi=200, bbox_inches='tight')
 
     plt.close(fig)
 
 
-def plot_histograms(dataset, vars, filename, nbins=60):
-
-    if not os.path.isdir(dataset):
-         os.mkdir(dataset)
-    
+def plot_oneD(dataset, vars, filename, bins=60):
+    """ Plot 1D marginalised posteriors for the 'vars' of interest."""
     n = len(vars)
 
     fig, axes = plt.subplots(nrows=n,
@@ -163,28 +70,46 @@ def plot_histograms(dataset, vars, filename, nbins=60):
                              sharex=False,
                              sharey=False)
 
-    plt.subplots_adjust(wspace=0, hspace=1)
-
     for i, x in enumerate(vars):
         ax = axes[i]
 
-        if x[0:6] == 'log(C_':
-            limits = (-30, 30)
-        else:
-            limits = None
-
-        P = posterior.oneD(dataset+'.h5', x, limits=limits, nbins=nbins)
-        P.marginalise()
+        P = posterior.oneD(dataset+'.h5', x, limits=limits(x), bins=bins)
         P.plot(ax)
+        ax.set_xlabel(labels(x))
 
         ax.set_yticklabels([])
 
-    fig.set_size_inches(4,n*2)
-    fig.savefig('%s/%s.png' % (dataset, filename), dpi=100)
+    fig.set_size_inches(4, 4*n)
+    fig.savefig(filename, dpi=200, bbox_inches='tight')
 
     plt.close(fig)
 
 
+# Define labels and the limits, or ranges, for the variables we're plotting.
+
+limits_dict = {
+    'log(m_{\chi})' : (0,3),
+    '\Omega_{\chi}h^2' : (0, 0.2),
+    'log(<\sigma v>)' : (-45, -20),
+    'log(\sigma_p^{SI})' : (-30, 10)
+}
+
+labels_dict = {
+    'log(m_{\chi})' : 'log10(mDM / 1 GeV)',
+    '\Omega_{\chi}h^2' : 'oh2',
+    'log(<\sigma v>)' : 'log10(<sv> / 1 cm^3 s^-1)',
+    'log(\sigma_p^{SI})' : 'log10(sigma_SI / 1 pb)'
+}
+
+for n in range(1,7):
+    limits_dict['log(C_%i)' % n] = (-30, 30)
+    labels_dict['log(C_%i)' % n] = 'log10(C%i / 1 GeV^-2)' % n
+
+def limits(x):
+    return None if x not in limits_dict else limits_dict[x]
+
+def labels(x):
+    return None if x not in labels_dict else labels_dict[x]
+
 if __name__ == '__main__':
 	main()
-
