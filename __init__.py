c08947787a7e64d34754c839c70bdebd860b89b4
Sebastian Liem <sebastian.liem@uva.nl>
2016-08-01 11:58:13 +0200

    Remove the top-level files.

--- __init__.py
+++
@@ -1,7 +0,0 @@
-
-import barrett.util as util
-import barrett.posterior as posterior
-import barrett.profilelikelihood as profilelikelihood
-import barrett.data as data
-
-__all__ = ['posterior', 'profilelikelihood', 'data', 'util']
